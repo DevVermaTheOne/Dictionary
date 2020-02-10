8a6456f7182234ab252b8ef2d4e0fada4cb1596c
Dev Verma <devvermatheone@gmail.com>
2020-02-10 21:04:33 +0530

    Faster Calculations

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import json
 from difflib import SequenceMatcher
+from difflib import get_close_matches
+
 
 data = json.load(open("data.json"))
 
@@ -11,13 +13,14 @@ def translate(w):
     if w in data:
       return data[w]
     else :
-        sim_ratio_old=0.0
-        for x in data :
-            sim_ratio_new=similar(w,x)
-            if(sim_ratio_new>sim_ratio_old):
-                sim_ratio_old=sim_ratio_new
-                key=x
-        print("Do you mean ", key, " ? (Answer in y/n)")
+        key=get_close_matches(w,data.keys())[0]
+    #    sim_ratio_old=0.0
+        #for x in data :
+        #    sim_ratio_new=similar(w,x)
+        #    if(sim_ratio_new>sim_ratio_old):
+        #        sim_ratio_old=sim_ratio_new
+        #        key=x
+        print("Do you mean", key,"? (Answer y to accept ")
         option=input()
         if(option == "y"):
             return  data[key]
